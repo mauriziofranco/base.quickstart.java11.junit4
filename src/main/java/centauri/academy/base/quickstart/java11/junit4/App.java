694466f18208a9a1d0d188e49f8dba7f8db2e4bf
MaurizioFranco <maurizio.franco@ymail.com>
2020-04-06 18:16:02 +0200

    Initial commit

---
+++ src/main/java/centauri/academy/base/quickstart/java11/junit4/App.java
@@ -0,0 +1,21 @@
+package centauri.academy.base.quickstart.java11.junit4;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	private boolean condition = true ;
+	
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+    
+    
+    public boolean getCondition ()
+    {
+        return condition ;
+    }
+}
